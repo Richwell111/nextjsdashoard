e34342b33d6a5943d3329f2a40d9f2b49cdd28ea
Richwell111 <bcict20103@gmail.com>
2025-10-06 13:23:48 +0000

    added the todo-list section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
-import { Button } from "@/components/ui/button";
+import TodoList from "@/components/TodoList";
+
+
 
 export default function Home() {
   return (
@@ -20,7 +22,7 @@ export default function Home() {
         <AppPieChart />
       </div>
 
-      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><TodoList/></div>
 
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
